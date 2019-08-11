b158cfa6fa0c81035dcae387bc63d1cf42a64bff
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-08-11 09:30:02 +0530

    modified js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
+    removeBorder();
     // Add border to current tab
     this.classList.add('tab-border');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
 // Listen for tab click
